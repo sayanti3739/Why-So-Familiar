9b650e25e275436842211228a2896b53b40c92ed
Sayanti Bhattacharjee <sayantib@microsoft.com>
2025-03-06 20:46:28 +0530

    updated cosmos uri

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.AZURE_COSMOS_CONNECTIONSTRING, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
